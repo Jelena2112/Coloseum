dbf2808af5429e130e481eacf9b0359bf6983886
tomislav.nikolic <tomislav.nikolic@mojagaraza.rs>
2018-05-27 18:07:36 +0200

    Admin rute

--- application/config/routes.php
+++ application/config/routes.php
@@ -9,7 +9,8 @@ $route['apartmani/(:any)'] = "apartmani_controller/poseban_apartman/$1";
 
 $route['contact'] = 'contact_controller';
 
-
+/* Admin rute */
+$route['admin'] = "admin_controller";
 
 $route['404_override'] = '';
 $route['translate_uri_dashes'] = FALSE;
