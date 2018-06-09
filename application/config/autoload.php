20658dd041d180da388eabde8a7aba600bf6d2fd
Jelena2112 <dijanic@gmail.com>
2018-06-09 19:28:05 +0200

    connect

--- application/config/autoload.php
+++ application/config/autoload.php
@@ -18,7 +18,7 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 | Instructions
 | -------------------------------------------------------------------
 |
-| These are the things you can load automatically:
+| These are the things you can load automatically:"
 |
 | 1. Packages
 | 2. Libraries
@@ -58,7 +58,7 @@ $autoload['packages'] = array();
 |
 |	$autoload['libraries'] = array('user_agent' => 'ua');
 */
-$autoload['libraries'] = array();
+$autoload['libraries'] = array("database");
 
 /*
 | -------------------------------------------------------------------
