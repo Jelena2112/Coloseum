524aaf83768c96d66406f9ea1dddf4d3312a1600
Toma <tomicwarr@gmail.com>
2018-06-09 23:56:57 +0200

    dsfdfsd

--- application/views/partials/header.php
+++ application/views/partials/header.php
@@ -1,22 +1,9 @@
-<nav class="navbar navbar-expand-lg navbar-light bg-light row">
-    <h1 class="navbar-brand col-sm-5 col-md-5 col-lg-8" href="home">Navbar</h1>
-    <button class="navbar-toggler col-sm-5 col-md-5 col-lg-8" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
-        <span class="navbar-toggler-icon"></span>
-    </button>
-    <div class="collapse navbar-collapse col-lg-4 col-sm-6" id="navbarNav">
-        <ul class="navbar-nav">
-            <li class="nav-item active">
-                <a class="nav-link" href="home">Home <span class="sr-only">(current)</span></a>
-            </li>
-            <li class="nav-item">
-                <a class="nav-link" href="about">About</a>
-            </li>
-            <li class="nav-item">
-                <a class="nav-link" href="apartmani">Apartments</a>
-            </li>
-            <li class="nav-item">
-                <a class="nav-link" href="contact">Contact</a>
-            </li>
-        </ul>
-    </div>
+<div>
+    <div>Leva</div>
+    <div>Desna</div>
+</div>
+
+<nav>
+    <section></section>
+    <section></section>
 </nav>
