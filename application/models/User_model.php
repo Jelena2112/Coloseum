69671de484aed11bbe327b7bc800a9e5e2a489a7
Jelena2112 <dijanic@gmail.com>
2018-06-09 20:25:54 +0200

    conn

--- application/models/User_model.php
+++ application/models/User_model.php
@@ -15,6 +15,14 @@ class User_model extends CI_Model
     {
         $query      = ['name' => $ime]; // SELECT * FROM users WHERE name = $ime
         $rezultat   = $this->db->get_where('users',$query);
+
+        if($rezultat->num_rows() < 1 )
+            return "Nije pronadjen korisnik $ime";
+
+
         return $rezultat->result();
     }
+
+
+
 }
